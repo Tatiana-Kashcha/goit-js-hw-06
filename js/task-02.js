c00be9bbc9c51bb8b906ead05379f58e1cdbcf06
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-14 19:24:11 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -10,8 +10,10 @@ const ingredientsList = document.querySelector("#ingredients");
 
 for (let i = 0; i < ingredients.length; i += 1) {
   const ingredient = ingredients[i];
+
   const ingredientsItem = document.createElement("li");
   ingredientsItem.textContent = ingredient;
+  ingredientsItem.classList.add("item");
   ingredientsList.append(ingredientsItem);
 }
 console.log(ingredientsList);
