229e174d6e880a7071ee283faeaa85f3bb4d7fb4
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-20 22:29:00 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", (event) => {
+  nameOutput.textContent = "Anonymous";
+  if (event.currentTarget.value) {
+    nameOutput.textContent = event.currentTarget.value;
+  }
+  console.log(nameOutput.textContent);
+});
