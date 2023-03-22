176fc6cd500acc7983c3d936f8648ef85b068819
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-22 20:46:45 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widgetEl = document.querySelector(".widget");
+const widgetColorSpan = widgetEl.children[0].firstElementChild;
+const widgetBtn = widgetEl.lastElementChild;
+
+const onWidgetBtnClick = () => {
+  widgetBtn.onclick = getRandomHexColor();
+  widgetColorSpan.textContent = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  widgetEl.style.backgroundColor = widgetColorSpan.textContent;
+};
+widgetBtn.addEventListener("click", onWidgetBtnClick);
