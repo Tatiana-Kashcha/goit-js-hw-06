545f1cf7249763132d84202b4c80bcbe1eabd2ee
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-22 23:44:04 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,20 +1,47 @@
-let counterValue = 0;
-const value = document.querySelector("#value");
+const counterEl = document.querySelector("#counter");
 
-const decrementBtn = document.querySelector(
-  '#counter button[data-action="decrement"]'
-);
+let counterValue = 0;
+const value = counterEl.children[1];
+
+const decrementBtn = counterEl.firstElementChild;
 const decrementClick = () => {
   counterValue -= 1;
   value.textContent = counterValue;
 };
 decrementBtn.addEventListener("click", decrementClick);
 
-const incrementBtn = document.querySelector(
-  '#counter button[data-action="increment"]'
-);
+const incrementBtn = counterEl.lastElementChild;
 const incrementClick = () => {
   counterValue += 1;
   value.textContent = counterValue;
 };
 incrementBtn.addEventListener("click", incrementClick);
+
+//---------------------------------------------------------------------------
+// 4TASK 2варіант
+
+// Проконсультуйте, будьласка, чи допустимо використовувати querySelector декілька разів ?
+// І чим це погано ? На лекції я почула, що не треба так робити, але зрозуміла не зовсім.
+// Тому цікавить Ваша думка з цього питання. Я спочатку так зробила, потім переробила після лекції.
+// Але мене збиває те, що в новому методі я не стукаюся до data-атрибутів, а просто використовую вкладеність.
+
+// let counterValue = 0;
+// const value = document.querySelector("#value");
+
+// const decrementBtn = document.querySelector(
+//   '#counter button[data-action="decrement"]'
+// );
+// const decrementClick = () => {
+//   counterValue -= 1;
+//   value.textContent = counterValue;
+// };
+// decrementBtn.addEventListener("click", decrementClick);
+
+// const incrementBtn = document.querySelector(
+//   '#counter button[data-action="increment"]'
+// );
+// const incrementClick = () => {
+//   counterValue += 1;
+//   value.textContent = counterValue;
+// };
+// incrementBtn.addEventListener("click", incrementClick);
