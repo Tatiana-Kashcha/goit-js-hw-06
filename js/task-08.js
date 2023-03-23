1497b481a07255b921de10848a109ebee00df2d6
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-23 20:09:14 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
 
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const user = { Email: email.value, Password: password.value };
+
+  console.log(user);
+  event.currentTarget.reset();
+}
+formEl.addEventListener("submit", onSubmit);
